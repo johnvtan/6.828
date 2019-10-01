299fcf34e4f26606c29e57c86a31a1f6f2d2faf5
John Vincent Tan <john.v.tan@gmail.com>
2019-09-30 22:33:09 -0400

    ex3 done?

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,4 +1,12 @@
+#include <inc/assert.h>
+#include <kern/pci.h>
 #include <kern/e1000.h>
 #include <kern/pmap.h>
 
 // LAB 6: Your driver code here
+int
+pci_e1000_attach(struct pci_func *pcif) {
+    cprintf("pci_e1000_attach called\n");
+    pci_func_enable(pcif);
+    return 0;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,3 +1,8 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
+
+#define E1000_VENDOR_ID (0x8086)
+#define E1000_DEVICE_ID (0x100E)
+
+int pci_e1000_attach(struct pci_func *pcif);
 #endif  // SOL >= 6
